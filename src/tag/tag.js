b352956210a336768b588370d7a94c88be41fc92
Celso Marciano <cemarte83@gmail.com>
2020-08-30 08:33:40 -0400

    Adds the tag plugin

---
+++ src/tag/tag.js
@@ -0,0 +1,10 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin'
+
+import TagEditing from './tagediting'
+import TagUI from './tagui'
+
+export default class Tag extends Plugin {
+  static get requires() {
+    return [TagEditing, TagUI];
+  }
+}
