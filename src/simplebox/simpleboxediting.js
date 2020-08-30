a11a58108eeceef0f6da9947820839df7dca5e1f
Celso Marciano <cemarte83@gmail.com>
2020-08-29 20:01:17 -0400

    Adds SimpleBox Schema

--- src/simplebox/simpleboxediting.js
+++ src/simplebox/simpleboxediting.js
@@ -3,5 +3,36 @@ import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
 export default class SimpleBoxEditing extends Plugin {
   init() {
     console.log("SimpleBoxEditing#init called");
+
+    this.defineSchema();
   }
+
+  defineSchema() {
+    const { schema } = this.editor.model;
+
+    schema.register("simpleBox", {
+      isObject: true,
+
+      allowWhere: "$block",
+    });
+  
+    schema.register("simpleBoxTitle", {
+      isLimit: true,
+
+      allowIn: 'simpleBox',
+
+      allowContentOf: "$block",
+    });
+
+    schema.register("simpleBoxDescription", {
+      isLimit: true,
+
+      allowIn: 'simpleBox', 
+
+      allowContentOf: "$root",
+    });
+
+  }
+
+  
 }
